7471ffa624aa9ddf5247c841bc87e38a3575b344
mayank-01-ms <69038296+mayank-01-ms@users.noreply.github.com>
2020-12-06 21:51:33 +0530

    Add files via upload

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,19 @@
 let hoursArea = document.getElementById("hours");
 let minutesArea = document.getElementById("minutes");
 let dateArea = document.getElementById("date");
+let tfArea = document.getElementById("tff");
 
 let hourHand = document.getElementById("hour-hand");
 let minuteHand = document.getElementById("minute-hand");
 let secondHand = document.getElementById("second-hand");
+
+let timeFormat = localStorage.getItem("timeFormat");
+
+if (timeFormat === null)
+    timeFormat = "twelve";
+
+if (timeFormat != "twelve")
+    tfArea.style.display = "none";
 
 let initiater = () => {
     let date = new Date();  
@@ -13,9 +22,14 @@ let initiater = () => {
     minutes = minutes < 10 ? '0' + minutes : minutes;
     let seconds = date.getSeconds();
     
+    if (timeFormat == "twelve"){
+        tfArea.innerHTML = hours > 12 ? "pm" : "am";
+        hours = hours > 12 ? hours - 12 : hours;
+    }
     hoursArea.innerHTML = hours;
     minutesArea.innerHTML = minutes;
     dateArea.innerHTML = date.toDateString();
+    
 
     hours = hours > 12 ? hours - 12 : hours;
     hourHand.style.transform = `rotate(${(hours*30)+(minutes/12)}deg)`;
